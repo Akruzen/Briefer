3895494756f207658e398da4e9b15efc8519922a
Akruzen <1omkarphadke@gmail.com>
2023-09-08 23:41:48 +0530

    Changed icon, added shortcuts.xml

---
+++ app/src/main/java/com/akruzen/briefer/QuickChatActivity.java
@@ -0,0 +1,14 @@
+package com.akruzen.briefer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuickChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_chat);
+    }
+}

19d60eaa58dfb9388cbb0087df695f77b9c947fd
Akruzen <1omkarphadke@gmail.com>
2023-09-10 12:40:38 +0530

    Populated QuickChatActivity UI

--- app/src/main/java/Constants/Methods.java
+++ app/src/main/java/Constants/Methods.java
@@ -2,8 +2,11 @@ package Constants;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Rect;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class Methods {
 
@@ -12,6 +15,21 @@ public class Methods {
         if (view != null) {
             InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public static void clearFocusEditText(Activity activity, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = activity.getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
         }
     }
 
--- app/src/main/java/com/akruzen/briefer/QuickChatActivity.java
+++ app/src/main/java/com/akruzen/briefer/QuickChatActivity.java
@@ -3,8 +3,17 @@ package com.akruzen.briefer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+
+import Constants.Methods;
 
 public class QuickChatActivity extends AppCompatActivity {
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Methods.clearFocusEditText(this, ev);
+        return super.dispatchTouchEvent(ev);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

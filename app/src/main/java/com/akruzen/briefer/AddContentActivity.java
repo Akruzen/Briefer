415f859020f14770a19af3c328f92c52f22b11aa
Akruzen <1omkarphadke@gmail.com>
2023-09-23 13:46:56 +0530

    Set character limit in add content to dynamic

--- app/src/main/java/com/akruzen/briefer/AddContentActivity.java
+++ app/src/main/java/com/akruzen/briefer/AddContentActivity.java
@@ -8,10 +8,12 @@ package com.akruzen.briefer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
@@ -22,6 +24,7 @@ import Constants.Constants;
 public class AddContentActivity extends AppCompatActivity {
 
     TextInputLayout titleTextInput, contentTextInput;
+    TextInputEditText contentTextInputEditText, questionTextInputEditText;
     TinyDB tinyDB;
 
     public void saveButtonClicked(View view) {
@@ -49,7 +52,20 @@ public class AddContentActivity extends AppCompatActivity {
         // Find views by id
         titleTextInput = findViewById(R.id.questionTextField);
         contentTextInput = findViewById(R.id.contentTextField);
+        contentTextInputEditText = findViewById(R.id.contentTextInput);
+        questionTextInputEditText = findViewById(R.id.questionTextInput);
         // Object Creation
         tinyDB = new TinyDB(this);
+        // Method Calls
+        setCharLimit();
+    }
+
+    private void setCharLimit() {
+        int charLimit = Integer.parseInt(tinyDB.getString(Constants.getCharLimitKey()));
+        titleTextInput.setCounterMaxLength(charLimit);
+        contentTextInput.setCounterMaxLength(charLimit);
+        InputFilter[] filterArray = new InputFilter[1];
+        filterArray[0] = new InputFilter.LengthFilter(charLimit);
+        contentTextInputEditText.setFilters(filterArray);
     }
 }
